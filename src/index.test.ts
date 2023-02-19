5c530150fcbd084f749f3df3b9d0d4458e714200
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-02-19 14:55:59 -0800

    Add initialize function

---
+++ src/index.test.ts
@@ -0,0 +1,14 @@
+import { initialize } from "@lib/index.js";
+import { describe, expect, it } from "vitest";
+
+describe("Initialize function", () => {
+  it("should be defined", () => {
+    expect(initialize).toBeDefined();
+    expect(initialize).toBeTypeOf("function");
+  });
+  it("should define google maps in the global scope", () => {
+    expect(global?.google?.maps).toBeUndefined();
+    initialize();
+    expect(global.google.maps).toBeDefined();
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const initialize = () => {
+  global.google = {
+    maps: {},
+  };
+};
